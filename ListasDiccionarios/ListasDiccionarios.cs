f8ed413a01b09882b38a0b8d46bac18ff96de912
Pedro Ulloa <pulloam@hotmail.com>
2022-11-10 18:52:39 -0300

    Agregar archivos de proyecto.

---
+++ ListasDiccionarios/ListasDiccionarios.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDiccionarios {
+    class ListasDiccionarios {
+        private List<string> losNombres = new List<string>();
+        private Dictionary<int, string> diccionarioNombres = new Dictionary<int, string>();
+
+        public void AgregarNombre(string nombre) {
+            losNombres.Add(nombre);
+        }
+
+        public void AgregarNombreDiccionario(int llave, string nombre) {
+            diccionarioNombres.Add(llave, nombre);
+        }
+
+        public void BuscarNombreDiccionario(int llave) {
+            string nombre = "";
+
+            diccionarioNombres.TryGetValue(llave, out nombre);
+
+            Console.WriteLine("\n------------------------\n");
+            Console.WriteLine("Buscando nombre con llave " + llave);
+            if(nombre != null) { 
+                Console.WriteLine("Nombre " + nombre);
+            } else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No existe llave en el diccionario");
+            }
+
+
+        }
+
+        public void EliminarNombre(int indice) {
+            Console.WriteLine("\n------------------------\n");
+            Console.WriteLine("Eliminando índice " + indice);
+            losNombres.RemoveAt(indice);
+        }
+
+        public void EliminarNombre(string nombre) {
+            Console.WriteLine("\n------------------------\n");
+            Console.WriteLine("Eliminando nombre " + nombre);
+            for (int x = 0; x < losNombres.Count;++x) {
+                if(losNombres[x] == nombre) {
+                    Console.WriteLine("Nombre eliminado en la posición " + x);
+                    losNombres.RemoveAt(x);
+                }
+            }
+        }
+
+        public void ObtenerNombresDiccionario() {
+            Console.WriteLine("\n------------------------\n");
+            Console.WriteLine("Recorrido del diccionario");
+            foreach (KeyValuePair<int, string> elemento in diccionarioNombres) {
+                Console.WriteLine("LLave : " + elemento.Key + " - Valor : " + elemento.Value);
+            }
+
+        }
+
+        public void ObtenerNombresLista() {
+            //Console.WriteLine("\n------------------------\n");
+
+            //Console.WriteLine("Recorrido con foreach");
+            //foreach (string nom in losNombres) {
+            //    Console.WriteLine("Nombre " + nom);
+            //}
+            Console.WriteLine("\n------------------------\n");
+            Console.WriteLine("Recorrido tradicional for");
+            for(int x = 0; x < losNombres.Count;++x) {
+                Console.WriteLine("Nombre en la posición " + x +" -> " + losNombres[x]);
+            }
+
+        }
+
+    }
+}
---
+++ ListasDiccionarios/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDiccionarios {
+    class Program {
+        static void Main(string[] args) {
+            ListasDiccionarios listaUno = new ListasDiccionarios();
+            ListasDiccionarios diccionarioUno = new ListasDiccionarios();
+
+            diccionarioUno.AgregarNombreDiccionario(111, "Juanito");
+            diccionarioUno.AgregarNombreDiccionario(222, "Maria");
+            diccionarioUno.AgregarNombreDiccionario(333, "Pepito");
+            diccionarioUno.AgregarNombreDiccionario(444, "Emilia");
+            diccionarioUno.AgregarNombreDiccionario(555, "Valentina");
+
+            diccionarioUno.ObtenerNombresDiccionario();
+
+            diccionarioUno.BuscarNombreDiccionario(555);
+            
+            //listaUno.AgregarNombre("Juanito");
+            //listaUno.AgregarNombre("Maria");
+            //listaUno.AgregarNombre("Pepito");
+            //listaUno.AgregarNombre("Emilia");
+            //listaUno.AgregarNombre("Valentina");
+
+            //listaUno.ObtenerNombresLista();
+
+            //listaUno.EliminarNombre(2);
+
+            //listaUno.EliminarNombre("Valentina");
+
+            //listaUno.ObtenerNombresLista();
+
+            Console.ReadKey();
+
+        }
+    }
+}
